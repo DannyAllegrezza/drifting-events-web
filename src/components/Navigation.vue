07f0851aa6c61a5e142f8b8d8dd13524596ad7cb
Danny Allegrezza <danny@dannyallegrezza.com>
2019-02-12 13:22:14 -0500

    add navbar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,53 +1,54 @@
 <template>
-<div id="app">
-    <section class="container">
-      <div class="has-text-centered">
-        <!-- <img src="./assets/logo.png"> -->
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <!-- Site Logo -->
+      <a href="#" class="navbar-item">
+        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" alt>
+      </a>
+      <!-- Hamburger menu -->
+      <a
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+        role="button"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
 
-      </div>
-      <hr>
-
-      <h1 class="title">Buefy Customization Basic Setup</h1>
-      <b-field>
-        <b-checkbox>Checkbox</b-checkbox>
-      </b-field>
-
-      <b-field>
-        <b-switch>Default</b-switch>
-      </b-field>
-
-      <b-field label="Name">
-        <b-input placeholder="John Silver"></b-input>
-      </b-field>
-
-      <div class="block">
-        <b-icon icon="person"></b-icon>
-        <b-icon icon="home"></b-icon>
-        <b-icon icon="dashboard"></b-icon>
-      </div>
-
-      <b-message title="Danger with icon" type="is-danger" has-icon>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id fermentum quam. Proin sagittis, nibh id hendrerit imperdiet, elit sapien laoreet elit
-      </b-message>
-
-      <b-field>
-        <b-dropdown>
-          <button class="button" slot="trigger">
-            <span>Filters</span>
-            <b-icon icon="arrow_drop_down"></b-icon>
-          </button>
-
-          <b-option value="open_issues">Open Issues and Pull Requests</b-option>
-          <b-option value="your_issues">Your Issues</b-option>
-          <b-option value="pull_requests">Your Pull Requests</b-option>
-          <b-option value="everything">Everything</b-option>
-        </b-dropdown>
-
+    <div class="navbar-menu" id="navbarBasicExample">
+      <!-- .navbar-start :: the left part of the menu, which appears next to the navbar brand on desktop -->
+      <div class="navbar-start">
+        <a class="navbar-item">Home</a>
+        <a class="navbar-item">About</a>
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">More</a>
+          <div class="navbar-dropdown">
+            <a class="navbar-item">About</a>
+            <a class="navbar-item">Jobs</a>
+            <a class="navbar-item">Contact</a>
+            <hr class="navbar-divider">
+            <a class="navbar-item">Report an issue</a>
+          </div>
+        </div>
         <b-input icon="search" type="search" placeholder="Search..."></b-input>
-      </b-field>
+      </div>
 
-    </section>
-  </div>
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a class="button is-light">Log in</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
